fccd745b47a5cfd0a58dfc1286d2912a7d721064
Max Lemieux <max.lemieux@gmail.com>
2020-05-27 15:29:12 -0700

    show employee pictures

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -1,14 +1,46 @@
-import React from 'react';
-const axios = require('axios');
+import React, { Component } from 'react';
+import axios from 'axios';
 
-async function employeeList (props) {
-  const employees = await axios.get("https://randomuser.me/api/?results=200&nat=us");
+class employeeList extends Component {
+  // const employees = axios.get("https://randomuser.me/api/?results=200&nat=us");
+  state = {
+    employees: [],
+  }
 
-  return (
-    <>
-    {employees}
-    </>
-  );
-}
+  componentDidMount() {
+    axios.get(`https://randomuser.me/api/?results=200&nat=us`)
+      .then(res => {
+        const employees = res.data.results;
+        this.setState({ employees });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+        {/* { this.state.employees } */}
+        {/* { console.log(this.state.employees) } */}
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+            </tr>
+            { this.state.employees.map(person => {
+              return (
+                <tr>
+                  <td><img src={person.picture.thumbnail} alt={person.name.first} /></td>
+                  <td>{person.name.last}, {person.name.first}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+};
 
 export default employeeList;
